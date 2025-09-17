7eb6bef9b8f781872c9454619dcc99023e7b6132
Preston Tang <prestonleetang0123@gmail.com>
2025-09-17 11:05:36 -0400

    Removed unnecessary uvicorn launch command

--- app/main.py
+++ app/main.py
@@ -3,7 +3,3 @@ from app.routes import router
 
 app = FastAPI(title="GridTanks API", version="1.0.0")
 app.include_router(router)
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=3007)
